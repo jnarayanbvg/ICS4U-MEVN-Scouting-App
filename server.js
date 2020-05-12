7ffdcd90a7c6fbed2dbfd6bb812d7080f22105bd
jnarayanbvg <38253333+jnarayanbvg@users.noreply.github.com>
2020-05-12 19:02:23 -0400

    Changed static folder

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.use('/api/comps', comps);
 // Handle production
 if(process.env.NODE_ENV === 'production') {
     //Static folder
-    app.use(express.static(__dirname + '/public/'));
+    app.use(express.static(__dirname + '/dist/'));
 
     //Handle single page applicaiton
-    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/dist/index.html'));
 }
 
 const port = process.env.PORT || 5000;
