da18cf304b6b8510e751aba3171ea7ac6b0460f1
jnarayanbvg <38253333+jnarayanbvg@users.noreply.github.com>
2020-05-15 10:53:00 -0400

    Reconfigured home components

--- src/components/Comps_Component.vue
+++ src/components/Comps_Component.vue
@@ -4,47 +4,21 @@
       class="comp"
       v-for="(comp) in comps"
       v-bind:key="comp._id"
-      v-on:dblclick="deleteComp(comp._id)"
+      v-on:dblclick="$emit('delete-comp', comp._id)"
     >
       <Comp v-bind:comp="comp" />
     </div>
-    <CreateComp v-on:create-comp="createComp" />
   </div>
 </template>
 
 <script>
-import CompService from '../CompService.js'
 import Comp from './Comp_Component.vue'
-import CreateComp from './CreateComp_Component.vue'
 
 export default {
   name: 'Comps',
   components: {
-    Comp,
-    CreateComp
+    Comp
   },
-  data() {
-    return {
-      comps: [],
-      error: ''
-    }
-  },
-  async created() {
-    try {
-      this.comps = await CompService.getComps();
-    } catch(err) {
-      this.error = err.message;
-    }
-  },
-  methods: {
-    async createComp(name) {
-      await CompService.createComp(name);
-      this.comps = await CompService.getComps();
-    },
-    async deleteComp(id) {
-      await CompService.deleteComp(id);
-      this.comps = await CompService.getComps();
-    }
-  }
+  props: ["comps"]
 }
 </script>
--- src/components/Home_Component.vue
+++ src/components/Home_Component.vue
@@ -2,17 +2,44 @@
   <div class="container_home">
     <h1>Your Newest FRC Competitions</h1>
     <hr />
-    <Comps />
+    <Comps v-bind:comps="comps" v-on:delete-comp="deleteComp" />
+    <CreateComp v-on:create-comp="createComp" />
   </div>
 </template>
 
 <script>
+import CompService from '../CompService.js'
 import Comps from './Comps_Component.vue'
+import CreateComp from './CreateComp_Component.vue'
 
 export default {
   name: 'Home',
   components: {
-    Comps
+    Comps,
+    CreateComp
+  },
+  data() {
+    return {
+      comps: [],
+      error: ''
+    }
+  },
+  async created() {
+    try {
+      this.comps = await CompService.getComps();
+    } catch(err) {
+      this.error = err.message;
+    }
+  },
+  methods: {
+    async createComp(name) {
+      await CompService.createComp(name);
+      this.comps = await CompService.getComps();
+    },
+    async deleteComp(id) {
+      await CompService.deleteComp(id);
+      this.comps = await CompService.getComps();
+    }
   }
 }
 </script>
